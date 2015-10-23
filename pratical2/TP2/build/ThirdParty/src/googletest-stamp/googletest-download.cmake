4314882ed6a836e94851929dd19965a3a174b851
Yoan LIARDET <yoan.liardet@poste.isima.fr>
2015-10-23 14:25:07 +0200

    ajout du dossier de build

---
+++ pratical2/TP2/build/ThirdParty/src/googletest-stamp/googletest-download.cmake
@@ -0,0 +1,20 @@
+
+
+set(command "/usr/bin/svn;co;http://googletest.googlecode.com/svn/trunk/;--non-interactive;googletest")
+execute_process(
+  COMMAND ${command}
+  RESULT_VARIABLE result
+  OUTPUT_FILE "/home/etud/liardety/Documents/MODL/tp2/TP2/build/ThirdParty/src/googletest-stamp/googletest-download-out.log"
+  ERROR_FILE "/home/etud/liardety/Documents/MODL/tp2/TP2/build/ThirdParty/src/googletest-stamp/googletest-download-err.log"
+  )
+if(result)
+  set(msg "Command failed: ${result}\n")
+  foreach(arg IN LISTS command)
+    set(msg "${msg} '${arg}'")
+  endforeach()
+  set(msg "${msg}\nSee also\n  /home/etud/liardety/Documents/MODL/tp2/TP2/build/ThirdParty/src/googletest-stamp/googletest-download-*.log\n")
+  message(FATAL_ERROR "${msg}")
+else()
+  set(msg "googletest download command succeeded.  See also /home/etud/liardety/Documents/MODL/tp2/TP2/build/ThirdParty/src/googletest-stamp/googletest-download-*.log\n")
+  message(STATUS "${msg}")
+endif()
